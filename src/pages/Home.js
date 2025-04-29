d53b5bef5268c96f7d90cd332cad3a87b895f1b9
John Doe <johndoe@example.com>
2025-04-30 01:16:28 +0300

    Fix background issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ function Home() {
   return (
     <PageLayout>
       <div className="hero-video-container">
-        <video autoPlay muted loop className="hero-video">
-          <source src="/Framenta.mp4" type="video/mp4" />
+        <video autoPlay muted loop playsInline className="hero-video">
+          <source src="/intro.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
