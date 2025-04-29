eae03b2e2a843417a67de716896802d45514813d
John Doe <johndoe@example.com>
2025-04-29 18:24:10 +0300

    fix page layout

--- src/pages/WhoWeAre.js
+++ src/pages/WhoWeAre.js
@@ -8,15 +8,21 @@ function Home() {
       <img
         src="/images/team.jpg"
         alt="Our Team"
-        style={{ maxWidth: "100%", borderRadius: "10px" }}
+        style={{ maxWidth: "70%", borderRadius: "30px" }}
       />
-      ;
+
       <p>
-        "At Framenta, Nemer, a full-stack developer specializing in front-end
-        development, leads a passionate team of web developers focused on
-        delivering high-quality, custom websites. We are dedicated to crafting
-        digital solutions that not only look amazing but also drive business
-        success by enhancing user experience and performance."
+        "At Framenta, a web design company under Reem Al Njoom, Nemer — a
+        full-stack developer specializing in front-end development — leads a
+        passionate team of web developers focused on delivering high-quality,
+        custom websites. We are dedicated to crafting digital solutions that not
+        only look amazing but also drive business success through enhanced user
+        experience, performance, and scalability. Our approach blends creativity
+        with cutting-edge technologies to ensure that every website we build is
+        responsive, SEO-friendly, and tailored to meet our clients’ unique
+        goals. Whether you're a startup or an established brand, we work closely
+        with you to transform your ideas into engaging digital experiences that
+        stand out in today’s competitive market."
       </p>
     </PageLayout>
   );
